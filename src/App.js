2e731a1f927f293171f17d6ab4c053a83296787f
Michael Mascarenhas <michael.mascarenhas@sagrupo.net>
2021-10-01 11:41:44 -0300

    form cadastro user sem style.css

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React from "react";
+import useForm from "./hooks/useForm";
+
+import "./style.css";
+
+const idList = [
+  "Vendedor",
+  "Analista de Crédito",
+  "AUX ADM",
+  "Analista Produção",
+  "Expedição",
+  "Faturista",
+  "ADMIN",
+  "Analista PCP",
+  "Vendedor P",
+  "Representante",
+];
+
+function App () {
+  const [{ values, loading }, handleChange, handleSubmit] = useForm();
+
+  const enviarContato = () => {
+    alert(JSON.stringify(values));
+  };
+
+  return (
+    <section className=".container-user">
+      <form className="form-user" onSubmit={handleSubmit(enviarContato)}>
+        <h1>Cadastro de Usuários</h1>
+        <div className="form-control">
+          <label htmlFor="cpf">
+            CPF do Novo Usuário:
+            <input
+              className=".input-user"
+              type="number"
+              name="cpf"
+              placeholder="CPF do Usúario"
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div className="form-control">
+          <label htmlFor="firstName">
+            Nome:
+            <input
+              className=".input-user"
+              type="text"
+              name="firstName"
+              placeholder="Primeiro Nome"
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="lastName">
+            Sobrenome:
+            <input
+              type="text"
+              name="lastName"
+              className=".input-user"
+              placeholder="Ultimo Nome"
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div className="form-control">
+          <label htmlFor="idType">
+            <select
+              name="idType"
+              className=".input-user"
+              onChange={handleChange}
+            >
+              {idList.map((item, i) => (
+                <option key={i}>{item}</option>
+              ))}
+            </select>
+          </label>
+        </div>
+        <div className="form-control">
+          <label htmlFor="protheusCode">
+            Codigo no Protheus:
+            <input
+              type="number"
+              name="protheusCode"
+              placeholder="Código do Protheus"
+              className=".input-user"
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">
+            Email do Usuário:
+            <input
+              type="text"
+              name="email"
+              placeholder="Email do Usúario"
+              className=".input-user"
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <button type="submit">{loading ? "Enviando..." : "Enviar"}</button>
+      </form>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/hooks/useForm.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+const useForm = () => {
+
+    const initialState = {
+        cpf: 0,
+        firstName: "",
+        lastName: "",
+        idType: "",
+        protheusCode: "",
+        email: "",
+    }
+
+    const [values, setValues] = useState({initialState});
+    const [loading, setLoading] = useState(false);
+
+    const handleChange = (event) => {
+        const auxValues = { ...values };
+        auxValues[event.target.name] = event.target.value;
+        setValues(auxValues);
+    };
+
+    const handleSubmit = (callback) => (event) => {
+        event.preventDefault();
+        setLoading(true);
+        callback();
+        setLoading(false);
+    };
+    
+    return [{ values, loading }, handleChange, handleSubmit];
+};
+
+export default useForm;
